c17876fde48cf77186882477bc52a1ec5b74ec70
Alex Kuzin <acidkarma3000@gmail.com>
2023-07-02 19:12:45 +0500

    Rework saving

--- src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
@@ -151,7 +151,7 @@ public class SavingAccountTest {
 
         account.pay(5_000);
 
-        Assertions.assertEquals(0, account.getBalance());
+        Assertions.assertEquals(5_000, account.getBalance());
     }
 
     @Test // Траты до баланса меньше нуля
